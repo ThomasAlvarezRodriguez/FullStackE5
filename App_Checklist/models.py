3a465a9f5d7fae8f6f2821aa8f3c09f4d8c1be2d
alexandredias34 <diasalex.340@gmail.com>
2023-11-25 17:52:31 +0100

    quetes

--- App_Checklist/models.py
+++ App_Checklist/models.py
@@ -27,11 +27,11 @@ class Item(models.Model):
 class Quete(models.Model):
     id = models.AutoField(primary_key=True)
     jeu = models.ForeignKey(Jeu, on_delete=models.CASCADE)
-    titre = models.CharField(max_length=100)
+    nom = models.CharField(max_length=100)
     description = models.TextField()
 
     def __str__(self):
-        return self.titre
+        return self.nom
 
 class ProfilUtilisateur(models.Model):
     id = models.AutoField(primary_key=True)
