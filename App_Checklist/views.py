b95a7e602ba4f0ef4d92307bb40a6c1ff5a3d025
alexandredias34 <diasalex.340@gmail.com>
2023-11-26 16:10:04 +0100

    test barre

--- App_Checklist/views.py
+++ App_Checklist/views.py
@@ -180,3 +180,19 @@ def update_progression(request, jeu_id):
         # If not a POST request, redirect to the game detail page without making changes
         return HttpResponseRedirect(reverse('game', args=[jeu_id]))
 
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import NewUserForm
+from django.contrib import messages
+
+def register(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful." )
+            return redirect("home")  # Redirect to a home page or login page
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+    form = NewUserForm()
+    return render(request, "register.html", {"register_form": form})
--- BackList/urls.py
+++ BackList/urls.py
@@ -37,6 +37,8 @@ urlpatterns = [
     path('favoris/<int:jeu_id>/', views.toggle_favoris, name='toggle_favoris'),
     path('toggle-obtenu-item/<int:item_id>/', views.toggle_obtenu_item, name='toggle_obtenu_item'),
     path('toggle-obtenu-quete/<int:quete_id>/', views.toggle_obtenu_quete, name='toggle_obtenu_quete'),
+    path('register/', views.register, name='register'),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
